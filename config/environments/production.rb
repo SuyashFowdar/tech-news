e7515626809eb38a1cc843313486e16c56d8dbc1
Suyash <suyash.krishna9@gmail>
2021-02-16 10:03:48 +0400

    Disable check precompiled asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.assets.check_precompiled_asset = false
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
