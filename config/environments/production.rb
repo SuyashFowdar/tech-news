ab48e454d670f1eb5311f934535a27b1074a6306
Suyash <suyash.krishna9@gmail>
2021-02-16 10:33:03 +0400

    Fix css issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.assets.check_precompiled_asset = false
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
